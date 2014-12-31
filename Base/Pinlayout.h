3103153f82cb669fe7d74f2b422ff1afb395b78b
Domokos Molnar <domokos@gmail.com>
2014-12-31 12:28:46 +0100

    Make all pind device type define dependent

--- Base/Pinlayout.h
+++ Base/Pinlayout.h
@@ -12,41 +12,36 @@
  *  This section defines the pins assignments the microcontroller
  */
 
-/* Watchdog pin
- * This - for PCB layoutting reasons - is:
- *
- *  P3_5 on the Master Controller
- *  P3_5 on the Six Owbus Device
- *  P3_3 on the Main panel device
- */
-
 #ifdef MASTER_CONTROLLER_DEVICE
   #define WATCHDOG_PIN P3_5
   #define COMM_DIRECTION_PIN P1_0
 #elif defined MAIN_PANEL_SLAVE_DEVICE
   #define WATCHDOG_PIN P3_3
   #define COMM_DIRECTION_PIN P1_7
+  // Define on which port the onewire interface is
+  #define ONEWIRE_IS_ON_P3
 #elif defined SIX_OWBUS_SLAVE_DEVICE
   #define WATCHDOG_PIN P3_5
   #define COMM_DIRECTION_PIN P1_7
+  // Define on which port the onewire interface is
+  #define ONEWIRE_IS_ON_P1
 #else
   #error "No comm direction pin could be determined"
 #endif
 
+#ifdef MAIN_PANEL_SLAVE_DEVICE
 // Define the pins of the SPI interface of the MCP4161
 #define PIN_NCS_HW P1_0
 #define PIN_NCS_HEAT P1_1
 #define PIN_SCK P3_7
 #define PIN_SDI_SDO P3_4
 
-// Define on which port the onewire interface is
-#define ONEWIRE_IS_ON_P3
-//#define ONEWIRE_IS_ON_P1
-
+// Define the pins of the Extender interface of the MCP4161
 #define DS_PIN P1_2
 #define NOE_PIN P1_3
 #define STCP_PIN P1_4
 #define SHCP_PIN P1_5
 #define NMR_PIN P1_6
+#endif
 
 #endif /* PINLAYOUT_H_ */
