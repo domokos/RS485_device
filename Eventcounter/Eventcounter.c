f5feb0a41ad5e6cac3d8caca53df566e727170d2
domokos <domokos@gmail.com>
2015-05-31 13:45:54 +0200

    Add empty eventcounter files to project

---
+++ Eventcounter/Eventcounter.c
@@ -0,0 +1,10 @@
+/*
+ * eventcounter.c
+ *
+ *  Created on: May 31, 2015
+ *      Author: dmolnar
+ */
+
+#include "Eventcounter.h"
+
+
---
+++ Eventcounter/Eventcounter.h
@@ -0,0 +1,14 @@
+/*
+ * eventcounter.h
+ *
+ *  Created on: May 31, 2015
+ *      Author: dmolnar
+ */
+
+#ifndef EVENTCOUNTER_H_
+#define EVENTCOUNTER_H_
+
+#include "Base.h"
+
+
+#endif /* EVENTCOUNTER_H_ */
